829776e024d8abdbfcb593d81e3a9b1d9a075a02
Slifer193 <holland.woodhe1@gmail.com>
2025-04-24 13:31:46 -0500

    IT RENDERS THE POSTS AUTOMATICALLY

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,18 @@
 const username = "admin";
 
-function renderPost(post) {
-    const template = document.getElementById("post-template").content.cloneNode(true);
+function renderPost(post, isNew = false) {
+    const template = document
+      .getElementById("post-template")
+      .content.cloneNode(true);
     template.querySelector(".username").innerText = post.username;
     template.querySelector(".message").innerText = post.message;
-    document.getElementById("feed").appendChild(template);
-}
+  
+    if (isNew) {
+      document.getElementById("feed").prepend(template);
+    } else {
+      document.getElementById("feed").appendChild(template);
+    }
+  }
 
 async function submitPost() {
     const message = document.getElementById("postInput").value;
@@ -20,6 +27,10 @@ async function submitPost() {
                 message: message
             }),
         });
+        if (response.ok) {
+            renderPost({ username, message }, true); // Pass `isNew = true`
+            document.getElementById("postInput").value = ""; // Clear the input box
+        }
     }catch (error){
         console.error("Cry About It 😭", error);
     }
